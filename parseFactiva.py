9b28d54d5ab56f5f69227d43bc7642e722e8f7fa
Paul Nulty <paul.nulty@gmail.com>
2014-06-08 19:38:55 +0100

    read_news

--- parseFactiva.py
+++ parseFactiva.py
@@ -1,9 +1,23 @@
-
+from os import listdir
+from os.path import isfile, join
 
 def read_news(dir):
-	pass
+	file_list = [ f for f in listdir(dir) if isfile(join(dir,f)) ]
+	for f in file_list:
+
+		doc = open(f).read()
 
 
-#main
+def parse_articles(doc):
+	articles = doc.split("____________\n")
+	print len(articles)
+	paper_codes = []
+	return
 
-d = 
+d = "C:\\Users\\Paul\\lsetemp\\LSEtext\\articles"
+
+fs = read_news(d)
+
+articles = []
+for f in fs:
+	articles.append(parse_articles(f))
