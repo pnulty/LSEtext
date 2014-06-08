e728d9cb8d4cf7a6ad2d1875dadc36ae2a97fe79
Paul Nulty <paul.nulty@gmail.com>
2014-06-08 22:48:19 +0100

    get lines

--- parseFactiva.py
+++ parseFactiva.py
@@ -1,26 +1,29 @@
+"""Read documents downloaded from factiva and split into articles
+with relevant attributes
+"""
+
 from os import listdir
 from os.path import isfile, join
 
-def read_news(di):
+def read_docs(di):
 	docs=[]
 	file_list = [join(di,f) for f in listdir(di) if isfile(join(di,f)) ]
 	for f in file_list:
 		docs.append(open(f).read())
 	return docs
 
-
-
-
-def parse_articles(doc):
+def split_docs(doc):
 	#print doc
 	articles = doc.split("____________\n")
-	print len(articles)
 	paper_codes = []
 	return articles
 
+def parse_article(art):
+	lines = art.split("\n")
+	print lines
+
 d = "C:\\Users\\Paul\\Dropbox\\LSETextMining\\code\\articles"
-docs = read_news(d)
-articles = []
-for d in docs:
-	articles.extend(parse_articles(d))
-print(len(articles))
+docs = read_docs(d)
+articles = [] 
+for d in docs: articles.extend(split_docs(d))
+for a in articles[4:15]: parse_article(a)
